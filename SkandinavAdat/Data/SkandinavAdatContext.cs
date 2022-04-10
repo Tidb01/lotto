282536962afbe3a8ca9766a98782367a19b68071
Forszt Mihály <mihalyforszt@outlook.hu>
2022-04-10 16:46:25 +0200

    projekt létrehozása

---
+++ SkandinavAdat/Data/SkandinavAdatContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkandinavAdat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkandinavAdat.Data
+{
+    public class SkandinavAdatContext: DbContext
+    {
+        public DbSet<LottoSzam> LottoSzamok { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;
+                                                     Database = LottoDB;
+                                               Initial Catalog = master;
+                                              Trusted_Connection = true"
+                                        );
+        }
+    }
+}
---
+++ SkandinavLottoFeltolt/Program.cs
@@ -0,0 +1,46 @@
+﻿using SkandinavAdat.Data;
+using SkandinavAdat.Models;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace SkandinavLottoFeltolt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SkandinavAdatContext db = new SkandinavAdatContext();
+
+            //if (!db.LottoSzamok.Any())
+            //{
+            string[] sorok = File.ReadAllLines("SkandinavLottoSzamok.csv").Skip(1).ToArray();
+            int[] tombSzam = new int[7];
+            foreach (var sor in sorok)
+            {
+                string[] tombSor = sor.Split(";");
+
+                for (int i = 0; i <= 7; i++)
+                {
+                    tombSzam[i] = int.Parse(tombSor[i]);
+                    db.LottoSzamok.Add(new LottoSzam(tombSzam));
+                }
+
+                for (int i = 0; i <= 7; i++)
+                {
+                    tombSzam[i] = int.Parse(tombSor[i + 7]);
+                    db.LottoSzamok.Add(new LottoSzam(tombSzam));
+                }
+            }
+            db.SaveChanges();
+            //}
+            //    else
+            //    {
+            //        Console.WriteLine("A Lottó Számok tábla már tartalmaz adatokat!");
+            //    }
+
+
+            Console.ReadKey();
+        }
+    }
+}
