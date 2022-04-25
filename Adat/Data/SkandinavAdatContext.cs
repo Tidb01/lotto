1653b8c28a8c458532cd96f658108f49f8bb4f06
Zsombor <tidb11111@gmail.com>
2022-04-25 20:29:56 +0200

    elkeszult

---
+++ Adat/Data/SkandinavAdatContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkandinavAdat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkandinavAdat.Data
+{
+    public class SkandinavAdatContext: DbContext
+    {
+        public DbSet<LottoSzam> LottoSzamok { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=lotto;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
